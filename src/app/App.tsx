66d77827440a21a941cd746c17aed0a502beca61
Mariano Britos <mbritos@q4tech.com>
2026-02-04 19:19:08 -0300

    Deshago cambio de ruteo.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,8 +16,7 @@ export default function App() {
         <Navigation />
         <main className="flex-grow">
           <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<Home />} />
+            <Route path="/" element={<Home />} />
             <Route path="/sport" element={<SportCatalog />} />
             <Route path="/work" element={<WorkCatalog />} />
             <Route path="/about" element={<About />} />
