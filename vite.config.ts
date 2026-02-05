feead580412eb946a3fc272a98a648d5c38cfc68
Mariano Britos <mbritos@q4tech.com>
2026-02-05 17:26:35 -0300

    Arreglo config vite para gh pages.

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-  base: '/home/',
+  base: '/el-vestuario-web/',
   resolve: {
     alias: {
       // Alias @ to the src directory
